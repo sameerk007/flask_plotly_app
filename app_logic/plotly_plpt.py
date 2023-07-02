a238a005009b10faf51eb20c60b31ce32e3a12ee
sameerk007 <rokzsam007@gmail.com>
2023-07-02 05:25:16 +0000

    minor fix

--- app_logic/plotly_plpt.py
+++ app_logic/plotly_plpt.py
@@ -5,7 +5,7 @@ import pandas as pd
 def get_plot(df: pd.DataFrame ,columns: list, title : str):
     fig = go.Figure()
     for column in columns:
-        fig.add_trace(go.Scatter(x=df['month_year'], y=df[column], name=column, showlegend=True,mode='markers+lines', marker=dict(size=5),line=dict(width=2)))
+        fig.add_trace(go.Scatter(x=df['month_year'].astype('string'), y=df[column], name=column, showlegend=True,mode='markers+lines', marker=dict(size=5),line=dict(width=2)))
     fig.update_layout(   
         title={
         'text': f'<b><i>{title}</i></b>',
